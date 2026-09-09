9d61cc9bfb78c2fcd64fea795482b67f2891948e
Julian Coy <jcloiacon@gmail.com>
2026-09-09 19:11:00 -0400

    Remove unused events CLI import
    [skip deploy]

--- scripts/events-cli.mjs
+++ scripts/events-cli.mjs
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import { readFile } from 'node:fs/promises'
 import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
 import { applyEventPlan, createEventsPlatform, registerEventsProvider } from './events-platform.mjs'
 import { LumaEventsProvider } from './luma-events-provider.mjs'
 
